e035e04d7c5fc2c8faeb000a7d97d7d39c638aee
sgriffith3 <sgriffith@alta3.com>
2021-10-25 17:06:44 +0000

    first example coming up

---
+++ myfirstscript.py
@@ -0,0 +1,12 @@
+print("Hello World") 
+print("Type something else") 
+print("Type something else again")
+
+x = 15
+if x > 10:
+    print("too big")
+    print(f"x is {x}")
+elif x < 5:
+    print("too small")
+else:
+    print("just right")
